06439132b46a0e2fd8ca764120542ce6730c87d8
Bishoeri000 <Bishoeri000@warren.k12.in.us>
2025-02-11 12:23:36 -0500

    Second Commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('Happy developing ✨')
+function validateForm() {
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    email.innerHTML = "";
+    password.innerHTML = "";
+    
+    if(email === "" && password === ""){
+        document.getElementById("error").innerText = "Email is required";
+        document.getElementById("error2").innerText = "Password is required";
+        return false;
+    }
+    
+    else if (email === "") {
+        document.getElementById("error").innerText = "Email is required";
+        return false;
+    }
+
+    else if (password === "") {
+        document.getElementById("error2").innerText = "Password is required";
+        return false;
+    }
+    
+    return true;
+}
